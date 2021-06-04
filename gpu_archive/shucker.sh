1bd2fb377129d58ac9768c2a17e8eddcfaf9d807
PhiYerion <PhiYerion@pm.me>
2021-06-04 19:09:47 +0000

    create 'shucker' & updating format of GPUs & making GPUs ordered & more GPUs

--- gpu_archive/shucker.sh
+++ gpu_archive/shucker.sh
@@ -4,16 +4,16 @@
 # at least until python port
 
 
-echo '( BACKUP FILE )' >> ~/Phi_DataBase/gpu_archive/roi_list.txt
+echo '( BACKUP FILE )' >> ~/Phi_DataBase/gpu_archive/roi_list.csv
 
-mv ~/Phi_DataBase/gpu_archive/roi_list.txt ~/Phi_DataBase/gpu_archive/.roi_list_bkp.txt
+mv ~/Phi_DataBase/gpu_archive/roi_list.txt ~/Phi_DataBase/gpu_archive/.roi_list_bkp.csv
 
 for i in {1..39}; do
-	line=""
+	line=`cat ~/Phi_DataBase/gpu_archive/gpu_format.txt|head -n $i|tail -n 1`
 	for file in `ls ~/Phi_DataBase/gpu_archive/history`; do
 		line+=`cat ~/Phi_DataBase/gpu_archive/history/$file|grep 'ROI'|tr -d '| avergROI     ~dys'|head -n $i|tail -n 1`
 	done
-	echo "$line" >> ~/Phi_DataBase/gpu_archive/roi_list.txt
+	echo "$line"|tr -s "	" "," >> ~/Phi_DataBase/gpu_archive/roi_list.csv
 done
 
 
