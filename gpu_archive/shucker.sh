d43fdbfffda1518a7d9ea2c6fc9e552a12bf6025
root <PhiYerion@pm.me>
2021-06-15 16:01:02 -0400

    more data

--- gpu_archive/shucker.sh
+++ gpu_archive/shucker.sh
@@ -13,7 +13,6 @@ for i in {1..39}; do
 	for file in `ls ~/Phi_DataBase/gpu_archive/history`; do
 		line+=`cat ~/Phi_DataBase/gpu_archive/history/$file|grep 'ROI'|tr -d '| avergROI     ~dys'|head -n $i|tail -n 1`
 	done
+	echo "$line"|tr -s "	" ","
 	echo "$line"|tr -s "	" "," >> ~/Phi_DataBase/gpu_archive/roi_list.csv
 done
-
-
