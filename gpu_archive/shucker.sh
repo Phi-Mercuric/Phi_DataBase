c24d4c73cfaa2ae6a710ec5636326985ee94b049
root <PhiYerion@pm.me>
2021-06-16 16:59:43 -0400

    more data

--- gpu_archive/shucker.sh
+++ gpu_archive/shucker.sh
@@ -11,8 +11,15 @@ mv ~/Phi_DataBase/gpu_archive/roi_list.csv ~/Phi_DataBase/gpu_archive/.roi_list_
 for i in {1..39}; do
 	line=`cat ~/Phi_DataBase/gpu_archive/gpu_format.txt|head -n $i|tail -n 1`
 	for file in `ls ~/Phi_DataBase/gpu_archive/history`; do
-		line+=`cat ~/Phi_DataBase/gpu_archive/history/$file|grep 'ROI'|tr -d '| avergROI     ~dys'|head -n $i|tail -n 1`
+		declare -i value
+		value=`cat ~/Phi_DataBase/gpu_archive/history/$file|grep 'ROI'|tr -d '| avergROI     ~dys'|head -n $i|tail -n 1`
+		if [[ $value -lt 50 ]]; then
+			value="3390"
+		elif [[ $value -gt 1000 ]]; then
+			value="replace"
+		fi
+		line+=,"$value"
+		
 	done
-	echo "$line"|tr -s "	" ","
 	echo "$line"|tr -s "	" "," >> ~/Phi_DataBase/gpu_archive/roi_list.csv
 done
